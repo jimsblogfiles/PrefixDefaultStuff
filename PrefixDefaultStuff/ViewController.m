a221eec77c23137cd7afc3519269d4812f991cbc
James Border <me@jamesborder.com>
2012-05-01 15:00:43 -0500

    Init remote commit?

--- PrefixDefaultStuff/ViewController.m
+++ PrefixDefaultStuff/ViewController.m
@@ -17,7 +17,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+	NSDate *date = [[NSDate alloc]init];
+
+	FuncLog(@"");	
+	BasicLog(@"BasicLog - %@",date);
+	UIAlertLog(@"Alert - %@",date);
+	
 }
 
 - (void)viewDidUnload
